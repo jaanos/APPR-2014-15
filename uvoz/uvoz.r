32794c2c3e60c5359e34e7a48b130b5c76a00fe7
Just Bajzelj <just.bajzelj@gmail.com>
2014-12-02 18:26:31 +0100

    uvazam podatke, program deluje verjetno

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,5 +1,8 @@
-branje.podatkov <- function(){
+branje.podatkov <- function(h){
   return(M<-read.csv2("podatki/film.csv", skip=0, na.strings="-", 
-                      fileEncoding="Windows-1250", col.names=c("mesto", "datum izida", "naslov filma", "Proracun", "Zasluzek v zda", "Zasluzek po svetu")
-  ))
+                      fileEncoding="Windows-1250", col.names=c("mesto", "datum izida", "naslov filma", "Proracun", "Zasluzek v zda", "Zasluzek po svetu")))
 }
+
+filmi<-branje.podatkov(h)
+
+
